e1a58710a2631910815921df8dc2642ab1e7d2c6
Den <39949334+shlvd@users.noreply.github.com>
2019-11-29 20:55:25 +0200

    Create valid_parentheses.py

---
+++ Easy/valid_parentheses.py
@@ -0,0 +1,9 @@
+def isValid(self, s: str) -> bool: 
+        parentheses = {"[": "]", "{": "}", "(": ")"}
+        stack = [] 
+        for par in s:
+            if par in parentheses:
+                stack.append(parentheses[par])
+            elif not stack or par != stack.pop():
+                return False
+        return not stack  
