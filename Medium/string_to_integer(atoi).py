2cc17b6b28555d253635f5cacdf13196c1093a30
Den <39949334+shlvd@users.noreply.github.com>
2019-11-29 21:15:15 +0200

    Update and rename 1 to string_to_integer(atoi).py

---
+++ Medium/string_to_integer(atoi).py
@@ -0,0 +1,25 @@
+def myAtoi(self, str):
+        INT_MAX, INT_MIN = 2**31 - 1, -2**31
+        str = str.lstrip(" ")
+        if not str: return 0
+        num = str.split(" ")[0]
+        result = ""
+        if num[0] == "-":
+            result += "-"
+            num = num[1:]
+        elif num[0] == "+": num = num[1:]
+        for char in num:
+            try:
+                x = int(char)
+                result += char
+            except:
+                break
+        try:
+            result = int(result)
+            if result in range(INT_MIN, INT_MAX):
+                return result
+            else:
+                if result < 0: return INT_MIN
+                else: return INT_MAX
+        except:
+            return 0
