b6c5c51a1f7ce773b7ef9be822e1448fb06ea904
Den <39949334+shlvd@users.noreply.github.com>
2019-11-30 21:36:30 +0200

    Create merge_two_sorted_lists.py

---
+++ Easy/merge_two_sorted_lists.py
@@ -0,0 +1,13 @@
+def mergeTwoLists(l1, l2):
+        head = tail = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next   
+           	
+        tail.next = l1 or l2
+        return head.next
