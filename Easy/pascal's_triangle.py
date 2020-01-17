3f84c88ae5c860dacfb8143af4fcba3defbb1a0b
Den <39949334+shlvd@users.noreply.github.com>
2020-01-17 22:48:34 +0200

    Create pascal's_triangle.py

---
+++ Easy/pascal's_triangle.py
@@ -0,0 +1,10 @@
+ def generate(numRows):
+        triangles = []
+        for i in range(numRows):
+            triangles.append([])
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    triangles[i].append(1)
+                else:
+                    triangles[i].append(triangles[i - 1][j - 1] + triangles[i - 1][j])
+        return triangles
