be63127059003ccd3990c58469d074b0eb170df8
Den <39949334+shlvd@users.noreply.github.com>
2020-02-12 13:12:57 +0200

    Create container_with_most_water.py

---
+++ Medium/container_with_most_water.py
@@ -0,0 +1,10 @@
+def maxArea(height):
+        area = 0
+        left, right = 0, len(height)-1
+        while left < right:
+            area = max(area, (right-left)*min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return area
