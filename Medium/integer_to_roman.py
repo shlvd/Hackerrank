bbf7cbec34050cc4f0a42e94d433d8f5ba3ab084
Den <39949334+shlvd@users.noreply.github.com>
2020-02-18 18:31:59 +0200

    Create integer_to_roman.py

---
+++ Medium/integer_to_roman.py
@@ -0,0 +1,10 @@
+def intToRoman(num):
+        lookup = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 
+                  'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
+        roman = ''
+        for symbol, val in sorted(lookup.items(), key = lambda t: t[1])[::-1]:
+            
+        	while num >= val:
+        		roman += symbol
+        		num -= val
+        return roman
