aff345ec6bc9c282bd87e97c8c1f19484064f7e1
Den <39949334+shlvd@users.noreply.github.com>
2020-03-05 20:48:01 +0200

    Create rotate_image.py

---
+++ Medium/rotate_image.py
@@ -0,0 +1,8 @@
+def rotate(matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
