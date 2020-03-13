6e824db5222a033f77a08a3bf5914d693a09f784
Den <39949334+shlvd@users.noreply.github.com>
2020-03-13 22:15:52 +0200

    Create jump_game.py

---
+++ Medium/jump_game.py
@@ -0,0 +1,8 @@
+def canJump(nums):
+        jump = nums[0]
+        for i in range(1,len(nums)):
+            if jump == 0:
+                return False
+            jump -= 1
+            jump = max(jump,nums[i])
+        return True
