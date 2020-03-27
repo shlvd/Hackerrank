27cdd47f1864a1ab96168c88551f21e0eb7c92c0
Den <39949334+shlvd@users.noreply.github.com>
2020-03-27 21:22:53 +0200

    Create largest_number.py

---
+++ Medium/largest_number.py
@@ -0,0 +1,6 @@
+def largestNumber(nums):
+        def compare(a,b):
+            return int(b + a) - int(a + b)
+        nums = sorted([str(x) for x in nums],key=cmp_to_key(compare))
+        ans = ''.join(nums).lstrip('0')
+        return ans or '0'
