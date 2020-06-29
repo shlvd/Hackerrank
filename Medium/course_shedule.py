46a3bbaf0b96c607530b17c508ef0fb7d93dfe29
Den <39949334+shlvd@users.noreply.github.com>
2020-06-29 12:47:17 +0300

    Create course_shedule.py

---
+++ Medium/course_shedule.py
@@ -0,0 +1,17 @@
+def canFinish(numCourses, prerequisites):
+        graph = collections.defaultdict(list)
+        indegrees = collections.defaultdict(int)
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegrees[u] += 1
+        for i in range(numCourses):
+            zeroDegree = False
+            for j in range(numCourses):
+                if indegrees[j] == 0:
+                    zeroDegree = True
+                    break
+            if not zeroDegree: return False
+            indegrees[j] = -1
+            for node in graph[j]:
+                indegrees[node] -= 1
+        return True                
