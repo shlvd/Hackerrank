b9153c4f403939db0989e08f9515a759d82bf5af
Den <39949334+shlvd@users.noreply.github.com>
2020-09-09 20:55:07 +0300

    Create reconstruct_itinerary.py

---
+++ Medium/reconstruct_itinerary.py
@@ -0,0 +1,14 @@
+def findItinerary(tickets):
+        def dfs(dep):
+            arr = paths[dep]
+            while arr:
+                dfs(arr.pop())
+            res.append(dep)
+
+        res = []
+        paths = defaultdict(list)
+        tickets.sort(key=lambda x: x[1], reverse=True)
+        for s, t in tickets:
+            paths[s].append(t)
+        dfs('JFK')
+        return res[::-1]
