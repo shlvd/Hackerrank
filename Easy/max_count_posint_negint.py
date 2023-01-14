c36462968eca63120322f7ada13cad7e1cec7ecb
Den <39949334+shlvd@users.noreply.github.com>
2023-01-14 23:54:20 +0200

    Create max_count_posint_negint.py

---
+++ Easy/max_count_posint_negint.py
@@ -0,0 +1,10 @@
+def maximumCount(self, nums: List[int]) -> int:
+        pos = neg = 0
+        for i in nums:
+            if i < 0:
+                pos += 1
+
+            if i > 0:
+                neg += 1
+
+        return max(pos, neg)
