d480636468bcb9a8c82890e6ff5c12a89687f6ef
Den <39949334+shlvd@users.noreply.github.com>
2023-01-24 23:57:07 +0200

    Create winner_university.sql

---
+++ Easy/winner_university.sql
@@ -0,0 +1,13 @@
+WITH nyu_cte AS (
+    SELECT COUNT(*) AS cnt FROM NewYork WHERE score >= 90
+), cu_cte AS (
+    SELECT COUNT(*) AS cnt FROM California WHERE score >= 90
+)
+
+SELECT
+    (CASE
+     WHEN N.cnt > C.cnt THEN 'New York University'
+     WHEN N.cnt < C.cnt THEN 'California University'
+     ELSE 'No Winner'
+     END) AS winner
+FROM nyu_cte N, cu_cte C;
