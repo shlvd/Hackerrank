90486c0efdb064cafc2679432e144b319ba4b446
Den <39949334+shlvd@users.noreply.github.com>
2023-02-20 19:08:33 +0200

    add new solution

--- Easy/search_insert_position.py
+++ Easy/search_insert_position.py
@@ -1,6 +1,14 @@
-def searchInsert(nums, target):
-        nums.append(target)         
-        nums.sort()                 
-        for i,j in enumerate(nums): 
-            if target == j:         
-                return i      
+def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        while right>=left:
+            mid = (left + right)//2
+
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left = mid+1
+            else:
+                right = mid-1
+                
+        return left
